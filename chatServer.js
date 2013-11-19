7e992bd40b088ac1ce8640a4d5026db1d9f7310d
naeluh <naeluh@gmail.com>
2013-11-19 21:15:37 +0000

    mod chatserver.js

--- chatServer.js
+++ chatServer.js
@@ -81,10 +81,10 @@ var sendBroadcast = function(text) {
     _.each(_.keys(io.sockets.manager.rooms), function(room) {
         room = room.substr(1); // Forward slash before room name (socket.io)
         // Don't send messages to default "" room
-        if (room) {
+
             var message = {'room':room, 'username':'ServerBot', 'msg':text, 'date':new Date()};
             io.sockets.in(room).emit('newMessage', message);
-        }
+
     });
     logger.emit('newEvent', 'newBroadcastMessage', {'msg':text});
 };
